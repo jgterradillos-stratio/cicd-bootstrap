3dcc1376eb5a8b68a6802913a1ae603f9977f1fb
Javier Garcia Terradillos <jgterradillos@stratio.com>
2026-06-10 09:46:18 +0200

    Load bootstrap file

--- vars/hose.groovy
+++ vars/hose.groovy
@@ -1,9 +1,24 @@
 def call(Closure body = {}) {
-    def folder = env.JOB_NAME.split('/')[0].toLowerCase()
-    if (folder == 'egeo') {
-        library "libpipelines@test/dynamic-library"
-    } else {
-        library "libpipelines@test/dynamic-library"
+
+    // Plugin: ConfigFileProvider  Method: configFileProvider
+    // Plugin: ConfigFileProvider  Method: configFile
+    def bootstrap
+    configFileProvider([configFile(fileId: 'libpipeline-bootstrap', variable: 'jsonFile')]) {
+        bootstrap = readJSON file: jsonFile
     }
+
+    echo "[hose] Bootstrap config loaded: ${bootstrap}"
+
+    def folder = env.JOB_NAME.split('/')[0].toLowerCase()
+    echo "[hose] Detected folder: ${folder}"
+
+    def libraryRef = bootstrap.find { lib, folders -> folders.contains(folder) }?.key
+    echo "[hose] Resolved library: ${libraryRef}"
+
+    if (!libraryRef) {
+        libraryRef = 'libpipeline'
+    }
+
+    library libraryRef
     hoseExecutor(body)
 }
