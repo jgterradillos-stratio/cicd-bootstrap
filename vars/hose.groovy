db68f37d6c8038be9af9ff8a13c83bb8043fe873
Javier Garcia Terradillos <jgterradillos@stratio.com>
2026-06-10 10:01:55 +0200

    Load bootstrap file

--- vars/hose.groovy
+++ vars/hose.groovy
@@ -11,14 +11,15 @@ def call(Closure body = {}) {
     def bootstrap = loadBootstrap()
     echo "[hose] Bootstrap config loaded: ${bootstrap}"
 
-    def folder = env.JOB_NAME.split('/')[0].toLowerCase()
+    def folder = env.JOB_NAME.split('/')[0]
+    
     echo "[hose] Detected folder: ${folder}"
 
     def libraryRef = bootstrap.find { lib, folders -> folders.contains(folder) }?.key
     echo "[hose] Resolved library: ${libraryRef}"
 
     if (!libraryRef) {
-        libraryRef = 'libpipeline'
+        libraryRef = 'libpipelines'
     }
 
     library libraryRef
